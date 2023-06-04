a6f5b13aebd77e342f0295b310f9ea17d35d77fa
Gabriel Moura <gmoura96@icloud.com>
2023-06-04 16:47:52 -0300

    Implementa controle de Arquivos para Eventos.

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -7,11 +7,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Activitylog\LogOptions;
 use Spatie\Activitylog\Traits\LogsActivity;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Event extends Model
+class Event extends Model  implements HasMedia
 {
     use HasFactory;
     use LogsActivity;
+    use InteractsWithMedia;
 
     protected $fillable = [
         'name',
