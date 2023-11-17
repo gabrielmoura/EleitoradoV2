bb0db6f066a430d8fd4533e3ceb036d65297ab55
Gabriel Moura <gmoura96@icloud.com>
2023-11-17 12:38:23 -0300

    Melhora exibição das notificações.

--- resources/views/dash/alerts/notifications.blade.php
+++ resources/views/dash/alerts/notifications.blade.php
@@ -4,42 +4,58 @@
         <div class="col-lg-12">
             <div class="row">
                 <div class="col">
-                    <div class="card shadow mb-3">
-                        <div class="card-body">
-                            <ul class="ovY-a pos-r scrollable lis-n p-0 m-0 fsz-sm">
-                                @foreach($alerts as $alert)
-                                    <li>
-                                        <a href="{{$alert->data['url']}}"
-                                           class='peers fxw-nw td-n p-20 bdB c-grey-800 cH-blue bgcH-grey-100'>
-                                            <input type="hidden" name="notification" id="notification_id">
-                                            <div class="peer mR-15">
-                                                @if(is_null($alert->read_at))
-                                                    <i class="fal fa-envelope fa-2x"></i>
-                                                @else
-                                                    <i class="fal fa-envelope-open fa-2x"></i>
-                                                @endif
-                                            </div>
-                                            <div class="peer mR-15">
-                                                <span>Por: System</span>
-                                            </div>
-                                            <span class="peer peer-greed">{{$alert->data['type']}}<span>
-                                            <span class="fw-500">{{$alert->data['text']}}</span>
-{{--                                            <span class="c-grey-600">{!! $alert->data['body'] !!}--}}
-                                            </span>
-                                                </span>
-                                            <p class="m-0">
-                                                <small
-                                                    class="fsz-xs">{{\Illuminate\Support\Carbon::parse($alert['created_at'])?->format('d/m/Y H:i')}}</small>
-                                            </p>
-                                            </span>
-                                        </a>
-                                    </li>
-                                @endforeach
-                            </ul>
-                        </div>
+                    <div class="d-block">
+                        @foreach($alerts as $alert)
+                            <div class="card">
+                                <div class="card-title">
+                                </div>
+                                <div class="card-body">
+                                    <div
+                                        class='peers fxw-nw td-n p-20 bdB c-grey-800 cH-blue bgcH-grey-100'>
+                                        <input type="hidden" name="notification" id="notification_id"
+                                               value="{{$alert->data['uid']}}">
+
+                                        <div class="peer mR-15">
+                                            @if(is_null($alert->read_at))
+                                                <i class="fal fa-envelope fa-2x"></i>
+                                            @else
+                                                <i class="fa-solid fa-envelope-open fa-2x"></i>
+                                            @endif
+                                            @if($alert->data['type']=='info')
+                                                <i class="fas fa-info-circle"></i>
+                                            @endif
+                                            @if($alert->data['type']=='warning')
+                                                <i class="fas fa-exclamation-triangle"></i>
+                                            @endif
+                                            @if($alert->data['type']=='danger')
+                                                <i class="fas fa-exclamation-circle"></i>
+                                            @endif
+                                            @if($alert->data['type']=='success')
+                                                <i class="fas fa-check-circle"></i>
+                                            @endif
+                                            <a class="btn btn-primary btn-sm" href="{{$alert->data['url']}}">
+                                                Abrir
+                                            </a>
+                                        </div>
+                                        <div class="peer mR-15">
+                                            <span>Por: System</span>
+                                        </div>
+                                        <span class="fw-500">Mensagem: {{$alert->data['text']}}</span>
+
+                                        <p class="m-0">
+                                            <small
+                                                class="fsz-xs">{{\Illuminate\Support\Carbon::parse($alert['created_at'])?->format('d/m/Y H:i')}}
+                                            </small>
+                                        </p>
+
+                                    </div>
+                                </div>
+                            </div>
+                        @endforeach
                     </div>
                 </div>
             </div>
         </div>
     </div>
+
 </x-app-layout>
