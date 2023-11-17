e432336beba6fd18a946fcacc3e462329dc101af
Gabriel Moura <gmoura96@icloud.com>
2023-11-17 12:39:15 -0300

    Define parâmetro de serialização e atualização de dependências.

--- config/database.php
+++ config/database.php
@@ -126,6 +126,8 @@ return [
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
             'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'serializer' => 3, // \Redis::SERIALIZER_MSGPACK
+//            'compression' => Redis::COMPRESSION_LZ4,
         ],
 
         'default' => [
