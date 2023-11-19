600b965df9c9af97b3dcc5d0ea50a5b2e34bcfde
Gabriel Moura <gmoura96@icloud.com>
2023-11-19 17:01:45 -0300

    Melhora a tela inicial.

--- resources/views/front/welcome.blade.php
+++ resources/views/front/welcome.blade.php
@@ -4,11 +4,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>{{config('app.name')}}</title>
+    <title>Eleitorado Digital</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet"/>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet"/>
 
     <!-- Styles -->
     <style>
@@ -368,21 +369,13 @@
             margin-top: 1rem
         }
 
-        .-mt-px {
-            margin-top: -1px
-        }
 
-        .mr-1 {
-            margin-right: 0.25rem
-        }
 
         .flex {
             display: flex
         }
 
-        .inline-flex {
-            display: inline-flex
-        }
+
 
         .grid {
             display: grid
@@ -392,41 +385,31 @@
             height: 4rem
         }
 
-        .h-7 {
-            height: 1.75rem
-        }
+
 
         .h-6 {
             height: 1.5rem
         }
 
-        .h-5 {
-            height: 1.25rem
-        }
+
 
         .min-h-screen {
             min-height: 100vh
         }
 
-        .w-auto {
-            width: auto
-        }
+
 
         .w-16 {
             width: 4rem
         }
 
-        .w-7 {
-            width: 1.75rem
-        }
+
 
         .w-6 {
             width: 1.5rem
         }
 
-        .w-5 {
-            width: 1.25rem
-        }
+
 
         .max-w-7xl {
             max-width: 80rem
@@ -458,9 +441,7 @@
             gap: 1.5rem
         }
 
-        .gap-4 {
-            gap: 1rem
-        }
+
 
         .self-center {
             align-self: center
@@ -512,9 +493,7 @@
             stroke: #ef4444
         }
 
-        .stroke-gray-400 {
-            stroke: #9ca3af
-        }
+
 
         .p-6 {
             padding: 1.5rem
@@ -536,6 +515,11 @@
         .text-xl {
             font-size: 1.25rem;
             line-height: 1.75rem
+        }
+
+        .text-5xl {
+            font-size: 3rem;
+            line-height: 1
         }
 
         .text-sm {
@@ -566,10 +550,7 @@
             color: rgb(107 114 128 / var(--tw-text-opacity))
         }
 
-        .underline {
-            -webkit-text-decoration-line: underline;
-            text-decoration-line: underline
-        }
+
 
         .antialiased {
             -webkit-font-smoothing: antialiased;
@@ -618,10 +599,7 @@
             color: rgb(17 24 39 / var(--tw-text-opacity))
         }
 
-        .hover\:text-gray-700:hover {
-            --tw-text-opacity: 1;
-            color: rgb(55 65 81 / var(--tw-text-opacity))
-        }
+
 
         .focus\:rounded-sm:focus {
             border-radius: 0.125rem
@@ -803,30 +781,26 @@
 
     <div class="max-w-7xl mx-auto p-6 lg:p-8">
         <div class="flex justify-center">
-        <img src="{{\Illuminate\Support\Facades\Vite::asset('resources/images/logo.png')}}" width="50px">
+        <span
+            class="text-5xl font-semibold text-gray-900 dark:text-white">Eleitorado Digital</span>
         </div>
 
         <div class="mt-16">
             <div class="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8">
-                <a href="https://laravel.com/docs"
-                   class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
+                <div
+                    class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
                     <div>
                         <div
                             class="h-16 w-16 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                 class="w-7 h-7 stroke-red-500">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                      d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25"/>
-                            </svg>
+
+                            <i class="fa fa-bullseye fa-2x text-red-500" style="color: #ef4444"></i>
                         </div>
 
-                        <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Documentation</h2>
+                        <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Segmentação Eficiente</h2>
 
                         <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-                            Laravel has wonderful documentation covering every aspect of the framework. Whether you are
-                            a newcomer or have prior experience with Laravel, we recommend reading our documentation
-                            from beginning to end.
-                        </p>
+                            CRM político permite segmentar eleitores com base em dados, otimizando estratégias de
+                            campanha para alcançar grupos específicos.
                     </div>
 
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
@@ -834,101 +808,68 @@
                         <path stroke-linecap="round" stroke-linejoin="round"
                               d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"/>
                     </svg>
-                </a>
-
-                <a href="https://laracasts.com"
-                   class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
-                    <div>
-                        <div
-                            class="h-16 w-16 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                 class="w-7 h-7 stroke-red-500">
-                                <path stroke-linecap="round"
-                                      d="M15.75 10.5l4.72-4.72a.75.75 0 011.28.53v11.38a.75.75 0 01-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25h-9A2.25 2.25 0 002.25 7.5v9a2.25 2.25 0 002.25 2.25z"/>
-                            </svg>
-                        </div>
-
-                        <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Laracasts</h2>
-
-                        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-                            Laracasts offers thousands of video tutorials on Laravel, PHP, and JavaScript development.
-                            Check them out, see for yourself, and massively level up your development skills in the
-                            process.
-                        </p>
-                    </div>
-
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                         class="self-center shrink-0 stroke-red-500 w-6 h-6 mx-6">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                              d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"/>
-                    </svg>
-                </a>
-
-                <a href="https://laravel-news.com"
-                   class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
-                    <div>
-                        <div
-                            class="h-16 w-16 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                 class="w-7 h-7 stroke-red-500">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                      d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 01-2.25 2.25M16.5 7.5V18a2.25 2.25 0 002.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 002.25 2.25h13.5M6 7.5h3v3H6v-3z"/>
-                            </svg>
-                        </div>
-
-                        <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Laravel News</h2>
-
-                        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-                            Laravel News is a community driven portal and newsletter aggregating all of the latest and
-                            most important news in the Laravel ecosystem, including new package releases and tutorials.
-                        </p>
-                    </div>
-
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                         class="self-center shrink-0 stroke-red-500 w-6 h-6 mx-6">
-                        <path stroke-linecap="round" stroke-linejoin="round"
-                              d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"/>
-                    </svg>
-                </a>
+                </div>
 
                 <div
                     class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
                     <div>
                         <div
                             class="h-16 w-16 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                 class="w-7 h-7 stroke-red-500">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                      d="M6.115 5.19l.319 1.913A6 6 0 008.11 10.36L9.75 12l-.387.775c-.217.433-.132.956.21 1.298l1.348 1.348c.21.21.329.497.329.795v1.089c0 .426.24.815.622 1.006l.153.076c.433.217.956.132 1.298-.21l.723-.723a8.7 8.7 0 002.288-4.042 1.087 1.087 0 00-.358-1.099l-1.33-1.108c-.251-.21-.582-.299-.905-.245l-1.17.195a1.125 1.125 0 01-.98-.314l-.295-.295a1.125 1.125 0 010-1.591l.13-.132a1.125 1.125 0 011.3-.21l.603.302a.809.809 0 001.086-1.086L14.25 7.5l1.256-.837a4.5 4.5 0 001.528-1.732l.146-.292M6.115 5.19A9 9 0 1017.18 4.64M6.115 5.19A8.965 8.965 0 0112 3c1.929 0 3.716.607 5.18 1.64"/>
-                            </svg>
+                            <i class="fa fa-mobile-alt fa-2x text-red-500" style="color: #ef4444"></i>
                         </div>
 
-                        <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Vibrant Ecosystem</h2>
+                        <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Engajamento Aprimorado</h2>
 
                         <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-                            Laravel's robust library of first-party tools and libraries, such as <a
-                                href="https://forge.laravel.com"
-                                class="underline hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Forge</a>,
-                            <a href="https://vapor.laravel.com"
-                               class="underline hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Vapor</a>,
-                            <a href="https://nova.laravel.com"
-                               class="underline hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Nova</a>,
-                            and <a href="https://envoyer.io"
-                                   class="underline hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Envoyer</a>
-                            help you take your projects to the next level. Pair them with powerful open source libraries
-                            like <a href="https://laravel.com/docs/billing"
-                                    class="underline hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Cashier</a>,
-                            <a href="https://laravel.com/docs/dusk"
-                               class="underline hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dusk</a>,
-                            <a href="https://laravel.com/docs/broadcasting"
-                               class="underline hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Echo</a>,
-                            <a href="https://laravel.com/docs/horizon"
-                               class="underline hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Horizon</a>,
-                            <a href="https://laravel.com/docs/sanctum"
-                               class="underline hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Sanctum</a>,
-                            <a href="https://laravel.com/docs/telescope"
-                               class="underline hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Telescope</a>,
-                            and more.
+                            Facilita a comunicação direta, personalizada e oportuna, fortalecendo o relacionamento
+                            político-eleitoral.
+                        </p>
+                    </div>
+
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                         class="self-center shrink-0 stroke-red-500 w-6 h-6 mx-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                              d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"/>
+                    </svg>
+                </div>
+
+                <div
+                    class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
+                    <div>
+                        <div
+                            class="h-16 w-16 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full">
+                            <i class="fa fa-chart-line fa-2x text-red-500" style="color: #ef4444"></i>
+                        </div>
+
+                        <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Análise Preditiva</h2>
+
+                        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+                            Utiliza dados para prever comportamentos eleitorais, possibilitando ajustes proativos nas
+                            estratégias de campanha.
+                        </p>
+                    </div>
+
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                         class="self-center shrink-0 stroke-red-500 w-6 h-6 mx-6">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                              d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"/>
+                    </svg>
+                </div>
+
+                <div
+                    class="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
+                    <div>
+                        <div
+                            class="h-16 w-16 bg-red-50 dark:bg-red-800/20 flex items-center justify-center rounded-full">
+                            <i class="fa fa-money-check fa-2x text-red-500" style="color: #ef4444"></i>
+                        </div>
+
+                        <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Gestão Efetiva de
+                            Doações</h2>
+
+                        <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
+                            Facilita o rastreamento transparente de doações, promovendo integridade financeira e
+                            conformidade regulatória.
                         </p>
                     </div>
                 </div>
@@ -936,19 +877,7 @@
         </div>
 
         <div class="flex justify-center mt-16 px-0 sm:items-center sm:justify-between">
-            <div class="text-center text-sm text-gray-500 dark:text-gray-400 sm:text-left">
-                <div class="flex items-center gap-4">
-                    <a href="https://github.com/sponsors/taylorotwell"
-                       class="group inline-flex items-center hover:text-gray-700 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                             class="-mt-px mr-1 w-5 h-5 stroke-gray-400 dark:stroke-gray-600 group-hover:stroke-gray-600 dark:group-hover:stroke-gray-400">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                  d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"/>
-                        </svg>
-                        Sponsor
-                    </a>
-                </div>
-            </div>
+
 
             <div class="ml-4 text-center text-sm text-gray-500 dark:text-gray-400 sm:text-right sm:ml-0">
                 Laravel v{{ Illuminate\Foundation\Application::VERSION }} (PHP v{{ PHP_VERSION }})
