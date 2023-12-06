f715db02aca3622dcede8f3f64b04d8fce87f8e4
Gabriel Moura <gmoura96@icloud.com>
2023-12-05 23:08:24 -0300

    Implementa whereLike e whenLike ao Eloquent Builder.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,9 @@ namespace App\Providers;
 
 use App\Mail\JobFailedMailable;
 use App\Models\Company;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Queue\Events\JobFailed;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Queue;
 use Illuminate\Support\Lottery;
@@ -36,5 +38,58 @@ class AppServiceProvider extends ServiceProvider
         });
 
         Feature::define('site-redesign', Lottery::odds(10, 100));
+
+        /**
+         * @description Implementa o whereLike no Eloquent Builder
+         *
+         * @param string|array $attributes
+         * @param string|null $searchTerm
+         * @return Builder
+         */
+        Builder::macro('whereLike', function (string|array $attributes, ?string $searchTerm) {
+            //@phpstan-ignore-next-line
+            $this->where(function (Builder $query) use ($attributes, $searchTerm) {
+                foreach (Arr::wrap($attributes) as $attribute) {
+                    $query->when(str_contains($attribute, '.'),
+                        function (Builder $query) use ($attribute, $searchTerm) {
+                            [$relationName, $relationAttribute] = explode('.', $attribute);
+                            $query->orWhereHas($relationName, function (Builder $query) use ($relationAttribute, $searchTerm) {
+                                $query->where($relationAttribute, 'LIKE', "%{$searchTerm}%");
+                            });
+                        },
+                        function (Builder $query) use ($attribute, $searchTerm) {
+                            $query->orWhere($attribute, 'LIKE', "%{$searchTerm}%");
+                        }
+                    );
+                }
+            });
+
+            return $this;
+        });
+
+        /**
+         * @description Implementa o whenLike no Eloquent Builder
+         *
+         * @param string|null $condition
+         * @param string $attribute
+         * @return Builder
+         */
+        Builder::macro('whenLike', function (?string $condition, string $attribute) {
+            //@phpstan-ignore-next-line
+            $this->where(function (Builder $query) use ($condition, $attribute) {
+                $query->when($condition, function (Builder $query) use ($attribute, $condition) {
+                    $query->when(str_contains($attribute, '.'), function (Builder $query) use ($attribute, $condition) {
+                        [$relationName, $relationAttribute] = explode('.', $attribute);
+                        $query->orWhereHas($relationName, function (Builder $query) use ($relationAttribute, $condition) {
+                            $query->where($relationAttribute, 'LIKE', "%{$condition}%");
+                        });
+                    }, function (Builder $query) use ($attribute, $condition) {
+                        $query->orWhere($attribute, 'LIKE', "%{$condition}%");
+                    });
+                });
+            });
+
+            return $this;
+        });
     }
 }
