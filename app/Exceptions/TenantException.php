d97ef062db4c855a080fe7b3dd1176bb48976ffb
Gabriel Moura <gmoura96@icloud.com>
2023-12-09 19:22:18 -0300

    Implementa Report em TenantException.

--- app/Exceptions/TenantException.php
+++ app/Exceptions/TenantException.php
@@ -4,12 +4,18 @@ namespace App\Exceptions;
 
 use App\Models\User;
 use DomainException;
+use Illuminate\Support\Facades\Log;
 use Throwable;
 
 class TenantException extends DomainException
 {
-    public function __construct(string $message = '', int $code = 0, Throwable $previous = null, public ?User $user = null)
+    public function __construct(string $message = '', int $code = 0, ?Throwable $previous = null, public ?User $user = null)
     {
         parent::__construct($message, $code, $previous);
+    }
+
+    public function report(): void
+    {
+        Log::error($this->getMessage());
     }
 }
